03e743c62ca1f52a38b04b11e44b78a25433201e
arturo.sanchez@mobileappscompany.us <asc_stb@hotmail.com>
2018-07-11 01:20:07 -0400

    Add my Zoo (completed)

--- Week3/Homework/Daily-2/MyZoo/app/src/main/java/com/example/user/myzoo/model/Adapter/LV_CategoriesAdapter.java
+++ Week3/Homework/Daily-2/MyZoo/app/src/main/java/com/example/user/myzoo/model/Adapter/LV_CategoriesAdapter.java
@@ -1,12 +1,14 @@
 package com.example.user.myzoo.model.Adapter;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.user.myzoo.R;
@@ -30,12 +32,32 @@ public class LV_CategoriesAdapter extends ArrayAdapter<Category> {
 
         //region initialize the views
         TextView tvCategoryDescription = convertView.findViewById(R.id.tvCategoryDescription);
+        ImageView ivCategory = convertView.findViewById(R.id.ivCategory);
         //endregion
 
         //region bind the views with the data
         Category category = getItem(position);
         tvCategoryDescription.setText(category.getCategoryDescription());
         tvCategoryDescription.setTag(category.getCategoryId());
+
+        switch (category.getCategoryId()) {
+            case 1:
+                ivCategory.setBackgroundResource(R.drawable.mammals);
+                break;
+            case 2:
+                ivCategory.setBackgroundResource(R.drawable.birds);
+                break;
+            case 3:
+                ivCategory.setBackgroundResource(R.drawable.fish);
+                break;
+            case 4:
+                ivCategory.setBackgroundResource(R.drawable.snake);
+                break;
+            default:
+                ivCategory.setBackgroundResource(R.drawable.zoo_wallpaper);
+                break;
+        }
+
         //endregion
 
         //return super.getView(position, convertView, parent);
--- Week3/Homework/Daily-2/MyZoo/app/src/main/java/com/example/user/myzoo/model/Adapter/RV_AnimalsAdapter.java
+++ Week3/Homework/Daily-2/MyZoo/app/src/main/java/com/example/user/myzoo/model/Adapter/RV_AnimalsAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.user.myzoo.R;
@@ -43,6 +44,50 @@ public class RV_AnimalsAdapter extends RecyclerView.Adapter<RV_AnimalsAdapter.Vi
         Animal animal = animals.get(position);
         holder.tvAnimalDescription.setText(animal.getAnimalDescription());
         holder.tvAnimalDescription.setTag(animal.getAnimalId());
+
+        /*switch (animal.getAnimalId()) {
+            case 1:
+                holder.ivAnimal.setBackgroundResource(R.drawable.bear);
+                break;
+            case 2:
+                holder.ivAnimal.setBackgroundResource(R.drawable.kangaroo);
+                break;
+            case 3:
+                holder.ivAnimal.setBackgroundResource(R.drawable.tiger);
+                break;
+            case 4:
+                holder.ivAnimal.setBackgroundResource(R.drawable.colibri);
+                break;
+            case 5:
+                holder.ivAnimal.setBackgroundResource(R.drawable.eagle);
+                break;
+            case 6:
+                holder.ivAnimal.setBackgroundResource(R.drawable.owl);
+                break;
+            case 7:
+                holder.ivAnimal.setBackgroundResource(R.drawable.bandedarcherfish);
+                break;
+            case 8:
+                holder.ivAnimal.setBackgroundResource(R.drawable.cichlid);
+                break;
+            case 9:
+                holder.ivAnimal.setBackgroundResource(R.drawable.koi);
+                break;
+            case 10:
+                holder.ivAnimal.setBackgroundResource(R.drawable.crocodile);
+                break;
+            case 11:
+                holder.ivAnimal.setBackgroundResource(R.drawable.snake2);
+                break;
+            case 12:
+                holder.ivAnimal.setBackgroundResource(R.drawable.tortoise);
+                break;
+
+            default:
+                holder.ivAnimal.setBackgroundResource(R.drawable.zoo_wallpaper);
+                break;
+        }*/
+
         holder.bind(animals.get(position), listener);
     }
 
@@ -54,17 +99,20 @@ public class RV_AnimalsAdapter extends RecyclerView.Adapter<RV_AnimalsAdapter.Vi
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         private final TextView tvAnimalDescription;
+        private final ImageView ivAnimal;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             //region initialize the views
             tvAnimalDescription = itemView.findViewById(R.id.tvAnimalDescription);
+            ivAnimal = itemView.findViewById(R.id.ivAnimal);
             //endregion
         }
 
         public void bind(final Animal animal, final OnItemClickListener listener) {
             itemView.setOnClickListener(new View.OnClickListener() {
-                @Override public void onClick(View v) {
+                @Override
+                public void onClick(View v) {
                     listener.onItemClick(animal);
                 }
             });
