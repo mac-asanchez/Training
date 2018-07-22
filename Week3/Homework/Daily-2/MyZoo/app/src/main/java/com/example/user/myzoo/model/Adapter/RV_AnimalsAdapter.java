c239ca72b328ce3c058224d864dc4373da75ca7e
arturo.sanchez@mobileappscompany.us <asc_stb@hotmail.com>
2018-07-22 00:55:00 -0400

    Update My Zoo - Add view type validation

--- Week3/Homework/Daily-2/MyZoo/app/src/main/java/com/example/user/myzoo/model/Adapter/RV_AnimalsAdapter.java
+++ Week3/Homework/Daily-2/MyZoo/app/src/main/java/com/example/user/myzoo/model/Adapter/RV_AnimalsAdapter.java
@@ -11,30 +11,35 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.user.myzoo.R;
+import com.example.user.myzoo.model.Constants;
 import com.example.user.myzoo.model.Entity.Animal;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 public class RV_AnimalsAdapter extends RecyclerView.Adapter<RV_AnimalsAdapter.ViewHolder> {
-    public interface OnItemClickListener {
-        void onItemClick(Animal item);
-    }
-
-    List<Animal> animals;
-    private final OnItemClickListener listener;
     public static final String TAG = RV_AnimalsAdapter.class.getSimpleName() + "_TAG";
+    private final OnItemClickListener listener;
+    List<Animal> animals;
 
     public RV_AnimalsAdapter(List<Animal> animals, OnItemClickListener listener) {
         this.animals = animals;
         this.listener = listener;
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        Animal animal = animals.get(position);
+        return (animal.getCategoryId() == 2) ? Constants.SecondLayout : Constants.FirstLayout;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Log.d(TAG, "onCreateViewHolder: ");
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.animal_list_item, parent, false);
+        int CurrentLayout = (viewType == Constants.FirstLayout) ? R.layout.animal_list_item : R.layout.animal_list_item2;
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(CurrentLayout, parent, false);
 
         return new ViewHolder(view);
     }
@@ -44,7 +49,10 @@ public class RV_AnimalsAdapter extends RecyclerView.Adapter<RV_AnimalsAdapter.Vi
         Log.d(TAG, "onBindViewHolder: " + holder.toString());
         Animal animal = animals.get(position);
         holder.tvAnimalDescription.setText(animal.getAnimalDescription());
-        holder.tvAnimalDescription.setTag(animal.getAnimalId());
+        holder.tvAnimalDescription.setTag(String.valueOf(animal.getAnimalId()));
+        if (holder.tvAnimalDetail != null) {
+            holder.tvAnimalDetail.setText(animal.getDetail());
+        }
 
         Picasso.get().load(animal.getImageUrl()).into(holder.ivAnimal);
 
@@ -56,16 +64,22 @@ public class RV_AnimalsAdapter extends RecyclerView.Adapter<RV_AnimalsAdapter.Vi
         return animals.size();
     }
 
+    public interface OnItemClickListener {
+        void onItemClick(Animal item);
+    }
+
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        private final TextView tvAnimalDescription;
         private final ImageView ivAnimal;
+        private final TextView tvAnimalDescription;
+        private final TextView tvAnimalDetail;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             //region initialize the views
             tvAnimalDescription = itemView.findViewById(R.id.tvAnimalDescription);
             ivAnimal = itemView.findViewById(R.id.ivAnimal);
+            tvAnimalDetail = itemView.findViewById(R.id.tvAnimalDetail);
             //endregion
         }
 
---
+++ Week3/Homework/Daily-2/MyZoo/app/src/main/java/com/example/user/myzoo/model/Constants.java
@@ -0,0 +1,6 @@
+package com.example.user.myzoo.model;
+
+public class Constants {
+    public static final int FirstLayout = 1;
+    public static final int SecondLayout = 2;
+}
